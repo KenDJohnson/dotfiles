fbcf53cd4b263dfbf430d79f8b51bcfb13f12f94
Ken Johnson <kdjohnso93@gmail.com>
2018-11-17 08:21:31 -0600

    fixing up setup script

--- setup.sh
+++ setup.sh
@@ -1,29 +1,34 @@
-#!bin/bash
+#!/bin/bash
 
 tmux_override=""
 vim_override=""
+pip_cmd="pip3 install --user git+git://github.com/powerline/powerline"
 if [ -f ~/.tmux.conf ] ; then
   read -p "Tmux conf already exists. Overwrite? (y/n)"; yn
   case $yn in 
-    [Yy]* ) tmux_override="-f";;
+    [Yy]* ) rm ~/.tmux.conf;;
     [Nn]* ) exit 1;;
     * ) echo "What?";;
   esac
 fi
-ln -s $tmux_override .tmux.conf ~/.tmux.conf
+cp .tmux.conf ~/.tmux.conf
 if [ -f ~/.vimrc ] ; then
   read -p "Vimrc already exists. Overwrite? (y/n)"; yn
   case $yn in 
-    [Yy]* ) vim_override="-f";;
+    [Yy]* ) rm ~/.vimrc;;
     [Nn]* ) exit 1;;
     * ) echo "What?";;
   esac
 fi
-ln -s $vim_override .vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 if ! which pip3 ; then
-  echo "install pip3, then run 'pip3 install --user powerline'"
+  echo "install pip3, then run '$pip_cmd'"
   exit 1;
 fi
-pip3 install --user git+git://github.com/powerline/powerline
+$pip_cmd
+echo 'PATH=$PATH:$HOME/.local/bin/' >> ~/.bashrc
+source ~/.bashrc
+powerline-deamon -q
+echo "Update the powerline location in ~/.tmux.conf"
